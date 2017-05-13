5de17560a017d7f805fdc86a87d817623651c247
Melech Mizrachi <melechmizrachi@gmail.com>
2017-05-12 19:19:54 -0700

    Updating env example for prod.

--- config/EnvExample.php
+++ config/EnvExample.php
@@ -11,4 +11,17 @@ use Valkyrja\Config\Env as ValkyrjaEnv;
  */
 class Env extends ValkyrjaEnv
 {
+    public const APP_ENV      = 'production';
+    public const APP_DEBUG    = false;
+    public const APP_URL      = 'localhost';
+    public const APP_TIMEZONE = 'UTC';
+    public const APP_VERSION  = '1 (ALPHA)';
+
+    public const CONSOLE_USE_CACHE_FILE = true;
+
+    public const CONTAINER_USE_CACHE_FILE = true;
+
+    public const EVENTS_USE_CACHE_FILE = true;
+
+    public const ROUTING_USE_ROUTES_CACHE_FILE = true;
 }
