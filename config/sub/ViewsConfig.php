5e43f8c2ca24655e96a4ee5749eb3c25014811be
Melech Mizrachi <melechmizrachi@gmail.com>
2017-05-20 17:08:29 -0700

    Fixing env and config for twig.

--- config/sub/ViewsConfig.php
+++ config/sub/ViewsConfig.php
@@ -3,22 +3,10 @@
 namespace config\sub;
 
 use Valkyrja\Config\Sub\ViewsConfig as ValkyrjaViewsConfig;
-use Valkyrja\Contracts\Config\Env;
 
 /**
  * Class ViewsConfig.
  */
 class ViewsConfig extends ValkyrjaViewsConfig
 {
-    /**
-     * ViewsConfig constructor.
-     *
-     * @param \Valkyrja\Contracts\Config\Env $env
-     */
-    public function __construct(Env $env)
-    {
-        parent::__construct($env);
-
-        $this->twig = new TwigViewsConfig($env);
-    }
 }
