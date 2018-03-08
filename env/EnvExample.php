09535131547be04add80a1bc40d6964ba65ea515
Melech Mizrachi <melechmizrachi@gmail.com>
2018-03-08 10:57:13 -0800

    Updating app with new framework changes.

--- env/EnvExample.php
+++ env/EnvExample.php
@@ -2,16 +2,159 @@
 
 namespace env;
 
-use Valkyrja\Env\Env as ValkyrjaEnv;
-
 /**
  * Class Env.
  */
-class Env extends ValkyrjaEnv
+class Env
 {
+    /**
+     * Application env variables.
+     */
     public const APP_ENV      = 'local';
     public const APP_DEBUG    = true;
     public const APP_URL      = 'localhost';
     public const APP_TIMEZONE = 'UTC';
     public const APP_VERSION  = '1 (ALPHA)';
+    // public const APP_KEY            = null;
+    // public const APP_PATH_REGEX_MAP = null;
+    //
+    // public const APP_CONTAINER  = null;
+    // public const APP_DISPATCHER = null;
+    // public const APP_EVENTS     = null;
+
+    /**
+     * Config env variables.
+     */
+    // public const CONFIG_PROVIDERS       = null;
+    // public const CONFIG_FILE_PATH       = null;
+    // public const CONFIG_CACHE_FILE_PATH = null;
+    // public const CONFIG_USE_CACHE_FILE  = null;
+
+    /**
+     * Annotation env variables.
+     */
+    // public const ANNOTATIONS_ENABLED   = null;
+    // public const ANNOTATIONS_CACHE_DIR = null;
+    // public const ANNOTATIONS_MAP       = null;
+
+    /**
+     * Console env variables.
+     */
+    // public const CONSOLE_PROVIDERS                   = null;
+    // public const CONSOLE_CORE_PROVIDERS              = null;
+    // public const CONSOLE_DEV_PROVIDERS               = null;
+    // public const CONSOLE_QUIET                       = null;
+    // public const CONSOLE_USE_ANNOTATIONS             = null;
+    // public const CONSOLE_USE_ANNOTATIONS_EXCLUSIVELY = null;
+    // public const CONSOLE_HANDLERS                    = null;
+    // public const CONSOLE_FILE_PATH                   = null;
+    // public const CONSOLE_CACHE_FILE_PATH             = null;
+    // public const CONSOLE_USE_CACHE_FILE              = null;
+
+    /**
+     * Container env variables.
+     */
+    // public const CONTAINER_PROVIDERS                   = null;
+    // public const CONTAINER_CORE_PROVIDERS              = null;
+    // public const CONTAINER_DEV_PROVIDERS               = null;
+    // public const CONTAINER_USE_ANNOTATIONS             = null;
+    // public const CONTAINER_USE_ANNOTATIONS_EXCLUSIVELY = null;
+    // public const CONTAINER_SERVICES                    = null;
+    // public const CONTAINER_CONTEXT_SERVICES            = null;
+    // public const CONTAINER_FILE_PATH                   = null;
+    // public const CONTAINER_CACHE_FILE_PATH             = null;
+    // public const CONTAINER_USE_CACHE_FILE              = null;
+
+    /**
+     * Crypt env variables.
+     */
+    // public const CRYPT_KEY      = null;
+    // public const CRYPT_KEY_PATH = null;
+
+    /**
+     * Database env variables.
+     */
+    // public const DB_CONNECTION = null;
+    // public const DB_HOST       = null;
+    // public const DB_PORT       = null;
+    // public const DB_DATABASE   = null;
+    // public const DB_USERNAME   = null;
+    // public const DB_PASSWORD   = null;
+    // public const DB_SOCKET     = null;
+    // public const DB_CHARSET    = null;
+    // public const DB_COLLATION  = null;
+    // public const DB_PREFIX     = null;
+    // public const DB_STRICT     = null;
+    // public const DB_ENGINE     = null;
+    // public const DB_SCHEME     = null;
+    // public const DB_SSL_MODE   = null;
+
+    /**
+     * Events env variables.
+     */
+    // public const EVENTS_USE_ANNOTATIONS             = null;
+    // public const EVENTS_USE_ANNOTATIONS_EXCLUSIVELY = null;
+    // public const EVENTS_CLASSES                     = null;
+    // public const EVENTS_FILE_PATH                   = null;
+    // public const EVENTS_CACHE_FILE_PATH             = null;
+    // public const EVENTS_USE_CACHE_FILE              = null;
+
+    /**
+     * Filesystem env variables.
+     */
+    // public const FILESYSTEM_DEFAULT    = null;
+    // public const FILESYSTEM_LOCAL_DIR  = null;
+    // public const FILESYSTEM_S3_KEY     = null;
+    // public const FILESYSTEM_S3_SECRET  = null;
+    // public const FILESYSTEM_S3_REGION  = null;
+    // public const FILESYSTEM_S3_VERSION = null;
+    // public const FILESYSTEM_S3_BUCKET  = null;
+    // public const FILESYSTEM_S3_DIR     = null;
+    // public const FILESYSTEM_S3_OPTIONS = null;
+
+    /**
+     * Logger env variables.
+     */
+    // public const LOGGER_NAME      = null;
+    // public const LOGGER_FILE_PATH = null;
+
+    /**
+     * Routing env variables.
+     */
+    // public const ROUTING_TRAILING_SLASH              = null;
+    // public const ROUTING_USE_ABSOLUTE_URLS           = null;
+    // public const ROUTING_USE_ANNOTATIONS             = null;
+    // public const ROUTING_USE_ANNOTATIONS_EXCLUSIVELY = null;
+    // public const ROUTING_CONTROLLERS                 = null;
+    // public const ROUTING_FILE_PATH                   = null;
+    // public const ROUTING_CACHE_FILE_PATH             = null;
+    // public const ROUTING_USE_CACHE_FILE              = null;
+
+    /**
+     * Session env variables.
+     */
+    // public const SESSION_ID   = null;
+    // public const SESSION_NAME = null;
+
+    /**
+     * Storage env variables.
+     */
+    // public const STORAGE_UPLOADS_DIR = null;
+    // public const STORAGE_LOGS_DIR    = null;
+
+    /**
+     * Views env variables.
+     */
+    // public const VIEWS_DIR   = null;
+    // public const VIEWS_PATHS = null;
+
+    /**
+     * Twig views env variables.
+     */
+    // public const TWIG_FILE_EXTENSION = null;
+    // public const TWIG_DIR            = null;
+    // public const TWIG_DIR_NS         = null;
+    // public const TWIG_DIRS           = null;
+    // public const TWIG_COMPILED_DIR   = null;
+    // public const TWIG_EXTENSIONS     = null;
 }
