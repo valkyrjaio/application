84660e2fd8e9144f737200b67a8654d72dfff61d
Melech Mizrachi <melechmizrachi@gmail.com>
2020-01-27 21:31:28 -0700

    Adding methods for example of what is overridable.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,12 +2,54 @@
 
 namespace App\Http;
 
+use Throwable;
 use Valkyrja\Http\NativeKernel;
+use Valkyrja\Http\Request;
+use Valkyrja\Http\Response;
 
 /**
  * Class Kernel.
  */
 class Kernel extends NativeKernel
 {
-    //
+    /**
+     * Handle a request.
+     *
+     * @param Request $request The request
+     *
+     * @throws Throwable
+     *
+     * @return Response
+     */
+    public function handle(Request $request): Response
+    {
+        return parent::handle($request);
+    }
+
+    /**
+     * Terminate the kernel request.
+     *
+     * @param Request  $request  The request
+     * @param Response $response The response
+     *
+     * @return void
+     */
+    public function terminate(Request $request, Response $response): void
+    {
+        parent::terminate($request, $response);
+    }
+
+    /**
+     * Run the kernel.
+     *
+     * @param Request $request The request
+     *
+     * @throws Throwable
+     *
+     * @return void
+     */
+    public function run(Request $request = null): void
+    {
+        parent::run($request);
+    }
 }
