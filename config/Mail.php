d028dcf041b877c78952138d11d17b8839c12f81
Melech Mizrachi <melechmizrachi@gmail.com>
2020-09-05 19:31:02 -0700

    Updating Mail module.

--- config/Mail.php
+++ config/Mail.php
@@ -6,10 +6,6 @@ namespace Config;
 
 use Valkyrja\Config\Constants\ConfigKeyPart as CKP;
 use Valkyrja\Config\Constants\EnvKey;
-use Valkyrja\Mail\Adapters\LogAdapter;
-use Valkyrja\Mail\Adapters\MailgunAdapter;
-use Valkyrja\Mail\Adapters\NullAdapter;
-use Valkyrja\Mail\Adapters\PHPMailerAdapter;
 use Valkyrja\Mail\Config\Config as Model;
 use Valkyrja\Mail\Constants\ConfigValue;
 
@@ -25,20 +21,23 @@ class Mail extends Model
      */
     public function __construct()
     {
-        $this->fromAddress = 'hello@example.com';
-        $this->fromName    = 'Example';
-        $this->adapter     = CKP::PHP_MAILER;
-        $this->adapters    = [
+        $this->default         = CKP::PHP_MAILER;
+        $this->adapters        = array_merge(ConfigValue::ADAPTERS, []);
+        $this->drivers         = array_merge(ConfigValue::DRIVERS, []);
+        $this->mailers         = [
             CKP::LOG        => [
-                CKP::DRIVER  => env(EnvKey::MAIL_LOG_DRIVER, LogAdapter::class),
+                CKP::ADAPTER => env(EnvKey::MAIL_LOG_ADAPTER, CKP::LOG),
+                CKP::DRIVER  => env(EnvKey::MAIL_LOG_DRIVER, CKP::DEFAULT),
                 // null will use default adapter as set in log config
-                CKP::ADAPTER => env(EnvKey::MAIL_LOG_ADAPTER, null),
+                CKP::LOGGER  => env(EnvKey::MAIL_LOG_LOGGER, null),
             ],
             CKP::NULL       => [
-                CKP::DRIVER => env(EnvKey::MAIL_NULL_DRIVER, NullAdapter::class),
+                CKP::ADAPTER => env(EnvKey::MAIL_NULL_ADAPTER, CKP::NULL),
+                CKP::DRIVER  => env(EnvKey::MAIL_NULL_DRIVER, CKP::DEFAULT),
             ],
             CKP::PHP_MAILER => [
-                CKP::DRIVER     => env(EnvKey::MAIL_PHP_MAILER_DRIVER, PHPMailerAdapter::class),
+                CKP::ADAPTER    => env(EnvKey::MAIL_PHP_MAILER_ADAPTER, CKP::PHP_MAILER),
+                CKP::DRIVER     => env(EnvKey::MAIL_PHP_MAILER_DRIVER, CKP::DEFAULT),
                 CKP::USERNAME   => env(EnvKey::MAIL_PHP_MAILER_USERNAME, ''),
                 CKP::PASSWORD   => env(EnvKey::MAIL_PHP_MAILER_PASSWORD, ''),
                 CKP::HOST       => env(EnvKey::MAIL_PHP_MAILER_HOST, 'smtp1.example.com;smtp2.example.com'),
@@ -46,13 +45,21 @@ class Mail extends Model
                 CKP::ENCRYPTION => env(EnvKey::MAIL_PHP_MAILER_ENCRYPTION, 'tls'),
             ],
             CKP::MAILGUN    => [
-                CKP::DRIVER  => env(EnvKey::MAIL_MAILGUN_DRIVER, MailgunAdapter::class),
+                CKP::ADAPTER => env(EnvKey::MAIL_MAILGUN_ADAPTER, CKP::MAILGUN),
+                CKP::DRIVER  => env(EnvKey::MAIL_MAILGUN_DRIVER, CKP::DEFAULT),
                 CKP::DOMAIN  => env(EnvKey::MAIL_MAILGUN_DOMAIN, ''),
                 CKP::API_KEY => env(EnvKey::MAIL_MAILGUN_API_KEY, ''),
             ],
         ];
-        $this->message     = CKP::DEFAULT;
-        $this->messages    = array_merge(ConfigValue::MESSAGES, []);
+        $this->defaultMessage  = CKP::DEFAULT;
+        $this->messageAdapters = array_merge(ConfigValue::MESSAGES, []);
+        $this->messages        = [
+            CKP::DEFAULT => [
+                CKP::ADAPTER      => CKP::DEFAULT,
+                CKP::FROM_ADDRESS => env(EnvKey::MAIL_FROM_ADDRESS, 'hello@example.com'),
+                CKP::FROM_NAME    => env(EnvKey::MAIL_FROM_NAME, 'Example'),
+            ],
+        ];
 
         parent::__construct([], true);
     }
