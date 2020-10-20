6c5165fefe8e90894edbd9d980c5b66078da1b5d
Melech Mizrachi <melechmizrachi@gmail.com>
2020-10-19 19:49:59 -0700

    Updating app Controller to use Valkyrja abstract controller.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 /**
  * Class Controller.
  */
-abstract class Controller
+abstract class Controller extends \Valkyrja\Routing\Support\Controller
 {
     // Implement application shared controller methods here then make sure all your controllers extends this one
 }
