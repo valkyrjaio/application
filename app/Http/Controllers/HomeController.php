c617f948e403e68c523cea5d0980cf55be2eb5da
Melech Mizrachi <melechmizrachi@gmail.com>
2025-05-27 17:09:35 -0700

    Update Routing to new Config.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,62 +4,45 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use Valkyrja\Container\Annotation\Service;
+use Valkyrja\Http\Message\Enum\RequestMethod;
 use Valkyrja\Http\Message\Factory\Contract\ResponseFactory;
 use Valkyrja\Http\Message\Response\Contract\Response;
-use Valkyrja\Http\Routing\Annotation\Route;
 use Valkyrja\Http\Routing\Attribute\Parameter;
-use Valkyrja\Http\Routing\Attribute\Route\Any;
-use Valkyrja\Http\Routing\Attribute\Route\Get;
-use Valkyrja\Http\Routing\Attribute\Route\Patch;
-use Valkyrja\Http\Routing\Attribute\Route\Post;
-use Valkyrja\Http\Routing\Attribute\Route\Put;
+use Valkyrja\Http\Routing\Attribute\Route;
+use Valkyrja\Http\Routing\Attribute\Route\RequestMethod\Any;
+use Valkyrja\Http\Routing\Attribute\Route\RequestMethod\Get;
 use Valkyrja\View\Factory\Contract\ResponseFactory as ViewResponseFactory;
 
 use function Valkyrja\app;
 
 /**
  * Class HomeController.
- *
- * @Route("path" = "/", "name" = "home")
- * @Service("id" = "Some\\Service\\Id")
- * @Service\Alias("id" = "Some\\Service\\Id", "name" = "homeController")
- * @Service\Context("id" = "Some\\Other\\Service\\Id", "service" = "From\\Some\\Package")
  */
-#[\Valkyrja\Http\Routing\Attribute\Route(path: '/', name: 'home')]
+#[Route(path: '/', name: 'home')]
 class HomeController extends \Valkyrja\Http\Routing\Controller\Controller
 {
     /**
      * Property routing example.
      *
      * @var string
-     *
-     * @Route("path" = "/property", "name" = "property")
      */
-    #[\Valkyrja\Http\Routing\Attribute\Route(path: '/property', name: 'property')]
+    #[Route(path: '/property', name: 'property')]
     public string $propertyRouting = 'Property Routing Example';
 
     /**
      * Static property routing example.
      *
      * @var string
-     *
-     * @Route("path" = "/static-property", "name" = "staticProperty")
-     * @Route\Any("path" = "/static-property-any", "name" = "staticProperty.any")
      */
-    #[\Valkyrja\Http\Routing\Attribute\Route(path: '/static-property', name: 'staticProperty')]
-    #[Any(path: '/static-property-any', name: 'staticProperty.any')]
+    #[Any]
+    #[Route(path: '/static-property', name: 'staticProperty')]
     public static string $staticPropertyRouting = 'Static Property Routing Example';
 
     /**
      * Welcome action.
      * - Example of a view being returned.
-     *
-     * @return Response
-     *
-     * @Route("path" = "/", "name" = "welcome")
      */
-    #[\Valkyrja\Http\Routing\Attribute\Route(path: '/', name: 'welcome')]
+    #[Route(path: '/', name: 'welcome')]
     public function welcome(ViewResponseFactory $responseFactory): Response
     {
         return $responseFactory->createResponseFromView('index');
@@ -68,12 +51,8 @@ class HomeController extends \Valkyrja\Http\Routing\Controller\Controller
     /**
      * Dynamic action.
      * - Example of a view being returned.
-     *
-     * @return Response
-     *
-     * @Route("path" = "/{dynamicValue}", "name" = "dynamicValue", "parameters" = [{"name" = "dynamicValue", "regex" = "[a-zA-Z]+"}])
      */
-    #[\Valkyrja\Http\Routing\Attribute\Route(path: '/{dynamicValue}', name: 'dynamicValue')]
+    #[Route(path: '/{dynamicValue}', name: 'dynamicValue')]
     #[Parameter(name: 'dynamicValue', regex: '[a-zA-Z]+')]
     public function dynamic(ViewResponseFactory $responseFactory): Response
     {
@@ -82,14 +61,9 @@ class HomeController extends \Valkyrja\Http\Routing\Controller\Controller
 
     /**
      * Home action.
-     *
-     * @param ResponseFactory $responseFactory
-     *
-     * @return Response
-     *
-     * @Route("path" = "/home", "name" = "home", "methods" = ["RequestMethod::GET"])
      */
-    #[Get(path: '/home', name: 'home')]
+    #[Get]
+    #[Route(path: '/home', name: 'home')]
     public function home(ViewResponseFactory $responseFactory): Response
     {
         return $responseFactory->createResponseFromView('home/home');
@@ -98,18 +72,11 @@ class HomeController extends \Valkyrja\Http\Routing\Controller\Controller
     /**
      * Application version action.
      * - Example of string being returned.
-     *
-     * @return Response
-     *
-     * @Route\Get("path" = "/version", "name" = "version")
-     * @Route\Post("path" = "/version", "name" = "version.post")
-     * @Route\Put("path" = "/version/put", "name" = "version.put")
-     * @Route\Redirect\Permanent\Put("path" = "/version", "to" = "/version/put", "name" = "version.put.redirect")
      */
-    #[Get(path: '/version', name: 'version')]
-    #[Post(path: '/version', name: 'version.post')]
-    #[Put(path: '/version', name: 'version.put')]
-    #[Patch(path: '/version', name: 'version.patch.redirect', to: '/version/patch')]
+    #[Route(path: '/version', name: 'version', methods: [RequestMethod::GET])]
+    #[Route(path: '/version', name: 'version.post', methods: [RequestMethod::POST])]
+    #[Route(path: '/version', name: 'version.put', methods: [RequestMethod::PUT])]
+    #[Route(path: '/version', name: 'version.patch.redirect', methods: [RequestMethod::PATCH], to: '/version/patch')]
     public static function version(ResponseFactory $responseFactory): Response
     {
         return $responseFactory->createResponse(app()->getVersion());
