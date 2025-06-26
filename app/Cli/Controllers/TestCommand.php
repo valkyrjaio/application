40fdff6e535ff47945d5a4b5388ecd0ad17c70a4
Melech Mizrachi <melechmizrachi@gmail.com>
2025-06-25 21:49:27 -0700

    Cli: Updating new CLI component.

--- app/Cli/Controllers/TestCommand.php
+++ app/Cli/Controllers/TestCommand.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace App\Cli\Controllers;
 
+use Valkyrja\Cli\Interaction\Message\Answer as AnswerMessage;
+use Valkyrja\Cli\Interaction\Message\Banner;
+use Valkyrja\Cli\Interaction\Message\Contract\Answer;
+use Valkyrja\Cli\Interaction\Message\Message;
+use Valkyrja\Cli\Interaction\Message\NewLine;
+use Valkyrja\Cli\Interaction\Message\Question;
 use Valkyrja\Cli\Interaction\Message\SuccessMessage;
 use Valkyrja\Cli\Interaction\Output\Contract\Output;
 use Valkyrja\Cli\Routing\Attribute\Command;
@@ -13,13 +19,50 @@ use Valkyrja\Cli\Routing\Attribute\Command;
  */
 class TestCommand extends Controller
 {
-    #[Command(name: 'test', description: 'Test Command', helpText: 'A command to test for the app')]
-    public function test(): Output
+    protected const string YES_ANSWER = 'yes';
+    protected const string NO_ANSWER  = 'no';
+
+    #[Command(
+        name: 'test',
+        description: 'Test command',
+        helpText: new Message('A command to showcase possibilities for commands.')
+    )]
+    public function run(): Output
     {
         return $this->outputFactory
             ->createOutput()
-            ->withMessages(
-                ...(new SuccessMessage('This is a test.'))->asBanner(),
+            ->withAddedMessages(
+                new Banner(new SuccessMessage('This is a success banner.')),
+            )
+            ->withAddedMessages(
+                new NewLine(),
+                new Question(
+                    text: 'This is a question, right?',
+                    callable: [$this, 'answered'],
+                    answer: new AnswerMessage(
+                        defaultResponse: self::NO_ANSWER,
+                        allowedResponses: [
+                            self::YES_ANSWER,
+                            self::NO_ANSWER,
+                        ]
+                    )
+                )
             );
+    }
+
+    /**
+     * Callback for once the question is answered.
+     */
+    public function answered(Output $output, Answer $answer): Output
+    {
+        if ($answer->getUserResponse() === self::YES_ANSWER) {
+            return $output
+                ->withAddedMessages(
+                    new Message('You answered yes!!!'),
+                )
+                ->writeMessages();
+        }
+
+        return $output;
     }
 }
