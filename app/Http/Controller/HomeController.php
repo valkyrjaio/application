0d8719bee12729d52cd502db2cc8ddaf16fcffe9
Melech Mizrachi <melechmizrachi@gmail.com>
2025-07-10 20:50:31 -0700

    Http Routing: Only allow route attributes on methods, and only allow parameter attributes on parameters. Updating Collector accordingly.

--- app/Http/Controller/HomeController.php
+++ app/Http/Controller/HomeController.php
@@ -7,56 +7,19 @@ namespace App\Http\Controller;
 use Valkyrja\Application\Contract\Application;
 use Valkyrja\Http\Message\Enum\RequestMethod;
 use Valkyrja\Http\Message\Factory\Contract\ResponseFactory;
+use Valkyrja\Http\Message\Response\Contract\JsonResponse;
 use Valkyrja\Http\Message\Response\Contract\Response;
+use Valkyrja\Http\Message\Response\Contract\TextResponse;
 use Valkyrja\Http\Routing\Attribute\Parameter;
 use Valkyrja\Http\Routing\Attribute\Route;
-use Valkyrja\Http\Routing\Attribute\Route\RequestMethod\Any;
-use Valkyrja\Http\Routing\Attribute\Route\RequestMethod\Get;
 use Valkyrja\Http\Routing\Constant\Regex;
 use Valkyrja\View\Factory\Contract\ResponseFactory as ViewResponseFactory;
 
 /**
  * Class HomeController.
  */
-#[Route(path: '/', name: 'home')]
 class HomeController extends Controller
 {
-    /**
-     * Const routing example.
-     *
-     * @var string
-     */
-    #[Route(path: '/version', name: 'version', requestMethods: [RequestMethod::GET])]
-    #[Route(path: '/version', name: 'version.post', requestMethods: [RequestMethod::POST])]
-    #[Route(path: '/version', name: 'version.put', requestMethods: [RequestMethod::PUT])]
-    public const string VERSION = Application::VERSION;
-
-    /**
-     * Property routing example.
-     *
-     * @var string
-     */
-    #[Route(path: '/property', name: 'property')]
-    public string $propertyRouting = 'Property Routing Example';
-
-    /**
-     * Static property routing example.
-     *
-     * @var string
-     */
-    #[Any]
-    #[Route(path: '/static-property', name: 'staticProperty')]
-    public static string $staticPropertyRouting = 'Static Property Routing Example';
-
-    /**
-     * Static property json routing example.
-     *
-     * @var array<array-key, string>
-     */
-    #[Any]
-    #[Route(path: '/json', name: 'json')]
-    public static array $jsonPropertyRouting = ['Static Property Routing Json Example'];
-
     /**
      * Welcome action.
      * - Example of a view being returned.
@@ -72,16 +35,19 @@ class HomeController extends Controller
      * - Example of a view being returned.
      */
     #[Route(path: '/{value}', name: 'dynamicValue')]
-    #[Parameter(name: 'value', regex: Regex::ALPHA)]
-    public function dynamic(ViewResponseFactory $responseFactory, string $value): Response
-    {
+    public function dynamic(
+        ViewResponseFactory $responseFactory,
+        #[Parameter(name: 'value', regex: Regex::ALPHA)]
+        string $value
+    ): Response {
         return $responseFactory->createResponseFromView('dynamic/dynamic', ['value' => $value]);
     }
 
     /**
      * Home action.
      */
-    #[Get]
+    #[Route\RequestMethod\Get]
+    #[Route\RequestMethod\Head]
     #[Route(path: '/home', name: 'home')]
     public function home(ViewResponseFactory $responseFactory): Response
     {
@@ -89,14 +55,22 @@ class HomeController extends Controller
     }
 
     /**
-     * Application version action.
-     * - Example of string being returned.
+     * Json action.
      */
-    #[Route(path: '/version-method', name: 'version-method', requestMethods: [RequestMethod::GET])]
-    #[Route(path: '/version-method', name: 'version-method.post', requestMethods: [RequestMethod::POST])]
-    #[Route(path: '/version-method', name: 'version-method.put', requestMethods: [RequestMethod::PUT])]
-    public static function version(Application $app, ResponseFactory $responseFactory): Response
+    #[Route(path: '/json', name: 'json')]
+    public function json(ResponseFactory $responseFactory): JsonResponse
     {
-        return $responseFactory->createResponse($app->getVersion());
+        return $responseFactory->createJsonResponse(['Json response example']);
+    }
+
+    /**
+     * Application version action.
+     */
+    #[Route(path: '/version', name: 'version', requestMethods: [RequestMethod::GET])]
+    #[Route(path: '/version', name: 'version.post', requestMethods: [RequestMethod::POST])]
+    #[Route(path: '/version', name: 'version.put', requestMethods: [RequestMethod::PUT])]
+    public static function version(Application $app, ResponseFactory $responseFactory): TextResponse
+    {
+        return $responseFactory->createTextResponse($app->getVersion());
     }
 }
