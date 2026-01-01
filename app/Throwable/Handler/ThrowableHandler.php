dc8e56f95cf761336dc9adeec306aa6781c3ac5f
Melech Mizrachi <melechmizrachi@gmail.com>
2026-01-01 16:23:24 -0700

    [Throwable] Rename the ThrowableHandler to WhoopsThrowableHandler for better clarity.

--- app/Throwable/Handler/ThrowableHandler.php
+++ app/Throwable/Handler/ThrowableHandler.php
@@ -2,9 +2,20 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the Valkyrja Framework package.
+ *
+ * (c) Melech Mizrachi <melechmizrachi@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace App\Throwable\Handler;
 
-use Valkyrja\Throwable\Handler\ThrowableHandler as ValkyrjaExceptionHandler;
+use Valkyrja\Throwable\Handler\WhoopsThrowableHandler as ValkyrjaExceptionHandler;
+
+use const E_ALL;
 
 /**
  * Class ThrowableHandler.
