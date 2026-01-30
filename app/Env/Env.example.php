1aefdd45174d73c94930e75c1079060f68f47779
Melech Mizrachi <melechmizrachi@gmail.com>
2026-01-29 21:40:26 -0700

    [Http] Move routing middleware from Middleware to Routing.

--- app/Env/Env.example.php
+++ app/Env/Env.example.php
@@ -2,12 +2,21 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the Valkyrja Framework package.
+ *
+ * (c) Melech Mizrachi <melechmizrachi@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace App\Env;
 
 use App\Orm\Entity\User;
 use App\Provider\ComponentProvider;
 use Valkyrja\Application\Env\Env as ValkyrjaEnv;
-use Valkyrja\Http\Routing\Middleware\ViewRouteNotMatchedMiddleware;
+use Valkyrja\Http\Routing\Middleware\RouteNotMatched\ViewRouteNotMatchedMiddleware;
 use Valkyrja\Http\Server\Middleware\LogThrowableCaughtMiddleware;
 use Valkyrja\Http\Server\Middleware\ViewThrowableCaughtMiddleware;
 
