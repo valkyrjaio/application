1323574b00f54fab39af3b2a38e6833127eaab08
Melech Mizrachi <melechmizrachi@gmail.com>
2026-03-20 15:26:02 -0700

    [Http] Add providers for cli specific services and routes.

--- app/src/App/Cli/Config.example.php
+++ app/src/App/Cli/Config.example.php
@@ -61,6 +61,7 @@ final readonly class Config extends ValkyrjaConfig
             ComponentClass::HTTP_MESSAGE,
             ComponentClass::HTTP_MIDDLEWARE,
             ComponentClass::HTTP_ROUTING,
+            ComponentClass::HTTP_ROUTING_CLI,
             ComponentClass::HTTP_SERVER,
             ComponentClass::LOG,
             ComponentProvider::class,
