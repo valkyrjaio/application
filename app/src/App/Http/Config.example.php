4863593611fe34b558618cd25d04401e30b0ac34
Melech Mizrachi <melechmizrachi@gmail.com>
2026-03-22 22:45:46 -0700

    [Http] Deprecate env in favor of config.

--- app/src/App/Http/Config.example.php
+++ app/src/App/Http/Config.example.php
@@ -18,20 +18,36 @@ use Valkyrja\Application\Constant\ComponentClass;
 use Valkyrja\Application\Data\HttpConfig as ValkyrjaConfig;
 use Valkyrja\Application\Kernel\Contract\ApplicationContract;
 use Valkyrja\Application\Provider\Contract\ProviderContract;
+use Valkyrja\Http\Middleware\Contract\RequestReceivedMiddlewareContract;
+use Valkyrja\Http\Middleware\Contract\RouteDispatchedMiddlewareContract;
+use Valkyrja\Http\Middleware\Contract\RouteMatchedMiddlewareContract;
+use Valkyrja\Http\Middleware\Contract\RouteNotMatchedMiddlewareContract;
+use Valkyrja\Http\Middleware\Contract\SendingResponseMiddlewareContract;
+use Valkyrja\Http\Middleware\Contract\TerminatedMiddlewareContract;
+use Valkyrja\Http\Middleware\Contract\ThrowableCaughtMiddlewareContract;
+use Valkyrja\Http\Middleware\Data\Contract\ConfigContract;
+use Valkyrja\Http\Server\Middleware\CacheResponseMiddleware;
 
-final class Config extends ValkyrjaConfig
+final class Config extends ValkyrjaConfig implements ConfigContract
 {
     /**
-     * @param non-empty-string                          $namespace
-     * @param non-empty-string                          $dir
-     * @param non-empty-string                          $version
-     * @param non-empty-string                          $environment
-     * @param non-empty-string                          $timezone
-     * @param non-empty-string                          $key
-     * @param non-empty-string                          $dataPath
-     * @param non-empty-string                          $dataNamespace
-     * @param class-string<ProviderContract>[]          $providers
-     * @param array<callable(ApplicationContract):void> $callbacks
+     * @param non-empty-string                                  $namespace
+     * @param non-empty-string                                  $dir
+     * @param non-empty-string                                  $version
+     * @param non-empty-string                                  $environment
+     * @param non-empty-string                                  $timezone
+     * @param non-empty-string                                  $key
+     * @param non-empty-string                                  $dataPath
+     * @param non-empty-string                                  $dataNamespace
+     * @param class-string<ProviderContract>[]                  $providers
+     * @param array<callable(ApplicationContract):void>         $callbacks
+     * @param class-string<RequestReceivedMiddlewareContract>[] $requestReceivedMiddleware
+     * @param class-string<RouteMatchedMiddlewareContract>[]    $routeMatchedMiddleware
+     * @param class-string<RouteNotMatchedMiddlewareContract>[] $routeNotMatchedMiddleware
+     * @param class-string<RouteDispatchedMiddlewareContract>[] $routeDispatchedMiddleware
+     * @param class-string<ThrowableCaughtMiddlewareContract>[] $throwableCaughtMiddleware
+     * @param class-string<SendingResponseMiddlewareContract>[] $sendingResponseMiddleware
+     * @param class-string<TerminatedMiddlewareContract>[]      $terminatedMiddleware
      */
     public function __construct(
         string $namespace = 'App',
@@ -58,6 +74,15 @@ final class Config extends ValkyrjaConfig
         array $callbacks = [
             [ComponentProvider::class, 'publish'],
         ],
+        public array $requestReceivedMiddleware = [
+            CacheResponseMiddleware::class,
+        ],
+        public array $routeMatchedMiddleware = [],
+        public array $routeNotMatchedMiddleware = [],
+        public array $routeDispatchedMiddleware = [],
+        public array $throwableCaughtMiddleware = [],
+        public array $sendingResponseMiddleware = [],
+        public array $terminatedMiddleware = [],
     ) {
         parent::__construct(
             namespace: $namespace,
