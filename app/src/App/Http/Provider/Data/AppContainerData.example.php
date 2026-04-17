6da3cdf8a45b56ffe96122fe0932570f530519ee
Melech Mizrachi <melechmizrachi@gmail.com>
2026-04-17 02:11:30 -0700

    [Data] Add empty data classes as examples.

---
+++ app/src/App/Http/Provider/Data/AppContainerData.example.php
@@ -0,0 +1,28 @@
+<?php
+
+// phpcs:ignoreFile
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Valkyrja Framework package.
+ *
+ * (c) Melech Mizrachi <melechmizrachi@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Http\Provider\Data;
+
+use Valkyrja\Container\Data\ContainerData;
+
+final readonly class AppContainerData extends ContainerData
+{
+    public function __construct()
+    {
+        parent::__construct(
+            aliases: [],
+        );
+    }
+}
